74f81ae7f875f6d18c3e7ff76c200593c263030c
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-13 16:03:58 -0400

    Updated functions and images.

---
+++ client/src/functions/auth.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const createOrUpdateUser = async (authtoken) => {
+  return await axios.post(
+    `${process.env.REACT_APP_API}/create-or-update-user`,
+    {},
+    {
+      headers: {
+        authtoken,
+      },
+    }
+  );
+};
+
+export const validateEmail = (emailAddress) => {
+  if (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(emailAddress)) {
+    return true;
+  } else {
+    return false;
+  };
+};
